51c0224fd8ba972c577458e7f8189eb7a058f6b5
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-09-14 19:01:31 -0400

    COMMENTS

--- src/abstract_op_codes.rs
+++ src/abstract_op_codes.rs
@@ -1,3 +1,19 @@
+// Abstract Op Codes 
+// Author: Jacob Mealey <jacobmealey.maine@gmail.com>
+//
+// Object: 
+//  The purpose of this file is to maintain the three types of op codes in 
+//  a MIPS architecture (Register, Immediate and Jump) each have a struct 
+//  has two member functions, as_bin and new
+//  ----- function definitions: -----
+//  as_bin() takes no arguments and returns an unsigned 32 bit int
+//  that is the binary representation of the given op code
+// 
+//  I think there must be a way to abstract the as_bin function so that the same
+//  one can be used for all three :)
+//  
+//  new() takes a different amount of arguments depending on the op code
+//  and returns the op code as a struct. 
 pub struct Register {
     rs : u32,
     rt : u32,
@@ -9,7 +25,7 @@ pub struct Register {
 impl Register {
     pub fn as_bin(&self) -> u32 {
         let mut bin : u32 = 0; 
-        // offset buy n bits and cutoff anything over 5 bits
+        // offset buy n bits and filter any bits that shouldn't be high
         bin |= (self.rs & 31) << 21; 
         bin |= (self.rt & 31) << 16;
         bin |= (self.rd & 31) << 11;
@@ -18,6 +34,9 @@ impl Register {
         return bin;
     }
     
+    // Register::new()
+    // rs - source register 1; rt - source register 2, rd desitination register,
+    // shamt - shift amount, func - Register function
     pub fn new(rs: u32, rt: u32, rd: u32, shamt: u32, func: u32) -> Register{
         return Register{
             rs: rs,
