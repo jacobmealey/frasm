4cd96c4ad8eb11a6cd5d55bfb4a74bfff6ead18d
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-09-15 19:21:01 -0400

    split I and J format functions

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,7 @@ use std::collections::HashMap;
 
 
 fn main() {
-    let opcodes : HashMap<&str, u32> = [
-        ("bltz", 1),
-        ("j", 2),
-        ("jal", 3),
-        ("beq", 4),
-        ("bne", 5),
-        ("blez", 6),
-        ("bgtz", 7),
+    let i_format : HashMap<&str, u32> = [
         ("addi", 8),
         ("addiu", 9),
         ("slti", 10),
@@ -19,33 +12,43 @@ fn main() {
         ("andi", 12),
         ("ori", 13),
         ("xori", 14),
+        ("sb", 40),
+        ("sh", 41),
+        ("sw", 43),
+        ("beq", 4),
+        ("bne", 5),
         ("lui", 15),
+        ("lhu", 37),
+        ("lw", 35),
+        ("swc1", 57),
+        ("swc2", 58),
+        ("sdc1", 61),
+        ("sdc2", 62),
+        ("ldc1", 53),
+        ("ldc2", 54),
+        ("lwc1", 49),
+        ("lwc2", 50),
+        ("blez", 6),
+        ("bgtz", 7),
         ("TLB", 16),
         ("FLPt", 17),
         ("lb", 32),
         ("lh", 33),
         ("lwl", 34),
-        ("lw", 35),
         ("lbu", 36),
-        ("lhu", 37),
         ("lwr", 38),
-        ("sb", 40),
-        ("sh", 41),
         ("swl", 42),
-        ("sw", 43),
         ("swr", 46),
         ("cache", 47),
         ("ll", 48),
-        ("lwc1", 49),
-        ("lwc2", 50),
         ("pref", 51),
-        ("ldc1", 53),
-        ("ldc2", 54),
         ("sc", 56),
-        ("swc1", 57),
-        ("swc2", 58),
-        ("sdc1", 61),
-        ("sdc2", 62)
+        ("bltz", 1),
+    ].iter().cloned().collect();
+
+    let j_format: HashMap<&str, u32> = [
+        ("j", 2),
+        ("jal", 3),
     ].iter().cloned().collect();
 
     let r_funcs : HashMap<&str, u32> = [
@@ -88,32 +91,32 @@ fn main() {
         ("tne", 54)
     ].iter().cloned().collect();
 
-    let line = "add 1 2 2";
+    let line = "addi 2 2 4";
 
     let split_line: Vec<&str> = line.split(' ').collect();
 
-    if opcodes.get(split_line[0]) != None {
-        println!("{} is opcode", split_line[0]);
-    }else{
-        println!("{} is R func", split_line[0]);
+    if i_format.get(split_line[0]) != None {
+        println!("{} is I format", split_line[0]);
+    }else if r_funcs.get(split_line[0]) != None {
         if split_line.len() == 4 {
             let func: u32 = *r_funcs.get(split_line[0]).unwrap();
             let rd: u32 = split_line[1].parse().unwrap();
             let rs: u32 = split_line[2].parse().unwrap();
             let rt: u32 = split_line[3].parse().unwrap();
             let reg = abstract_op_codes::Register::new(rs, rt, rd, 0, func);
-            println!("Register: {:#010x}", reg.as_bin());
+            println!("{} --> {:#010x}", line, reg.as_bin());
         }
+    } else {
+        println!("{} is J format", split_line[0]);
     }
 
 
-    println!("Hello, world!");
-    let reg = abstract_op_codes::Register::new(0, 0, 2, 0, 37);
-    let imm = abstract_op_codes::Immediate::new(4, 8, 0, 3);
-    let jmp = abstract_op_codes::Jump::new(2, 0x100001);
-    println!("Register: {:#010x}", reg.as_bin());
-    println!("Immediate: {:#010x}", imm.as_bin());
-    println!("Jump: {:#010x}", jmp.as_bin());
-    println!("{}", opcodes.get("j").unwrap());
-    println!("{}", r_funcs.get("add").unwrap());
+    // let reg = abstract_op_codes::Register::new(0, 0, 2, 0, 37);
+    // let imm = abstract_op_codes::Immediate::new(4, 8, 0, 3);
+    // let jmp = abstract_op_codes::Jump::new(2, 0x100001);
+    // println!("Register: {:#010x}", reg.as_bin());
+    // println!("Immediate: {:#010x}", imm.as_bin());
+    // println!("Jump: {:#010x}", jmp.as_bin());
+    // println!("{}", opcodes.get("j").unwrap());
+    // println!("{}", r_funcs.get("add").unwrap());
 }
