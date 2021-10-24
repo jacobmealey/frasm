d1213b05dc9030e592252dc78834b125fa2b531f
Jacob Mealey <jacobmealey.maine@gmail.com>
2021-10-24 16:05:17 -0400

    added dollarsign for register support

--- src/main.rs
+++ src/main.rs
@@ -34,23 +34,29 @@ fn main() {
 
 
     // preprocess pipeline
-    // stage 1 -- filter comments and trailing whitespace and newlines
+    // stage 1 -- filter comments and trailing whitespace, newlines and dollarsigns
+    println!("Filtering trailing whitespace, newlines and dollarsigns.");
     let mut post_stage1: Vec<&str> = Vec::new();
     for line in &lines {
         let split: Vec<&str> = line.split('#').collect();
         if is_blank(split[0]) {                 // empty line
             continue;
-        } else if split.len() > 1 {             // ends in a comment
-            post_stage1.push(split[0].trim());
-        }else{                                  // regular line
-            post_stage1.push(split[0].trim());
-        }
+        }                                       // ends in a comment
+        post_stage1.push(split[0].clone().trim());
     }  
     
+    let mut post_dollar_signs: Vec<String> = Vec::new();
+    for line in &post_stage1 {
+        let rg = Regex::new(r"\$").unwrap();
+        let replaced = rg.replace(line, "".to_string()).to_string();
+        post_dollar_signs.push(replaced);
+    }
+
+    println!("Generating list of labels.");
     // stage 2 -- generate list of labels 
     let mut post_stage2: Vec<&str> = Vec::new();
     let mut labels: HashMap<&str, u32> = HashMap::new();
-    for line in &post_stage1 {
+    for line in &post_dollar_signs{
         let split: Vec<&str> = line.split(':').collect();
         line_count = line_count + 1;
         if split.len() > 1 {                    // line starts with a branch
@@ -62,9 +68,7 @@ fn main() {
 
     }
     
-    println!("post_stage2 len: {}", post_stage2.len());
-    println!("label count: {}", labels.keys().len());
-
+    println!("Replacing labels with integer value");
     // Go through and replace all the labels.
     let mut stage_2_post_labels: Vec<String> = Vec::new();
     for label in labels.keys() {
@@ -72,7 +76,6 @@ fn main() {
         let mut replaced_line: String;
         for lines in &post_stage2 {
             replaced_line = rg.replace(lines, labels[label].to_string()).to_string();
-            println!("{}", replaced_line);
             stage_2_post_labels.push(replaced_line);
         }
     }
@@ -114,6 +117,8 @@ fn main() {
         ("fp", 30),
         ("ra", 31),
     ].iter().cloned().collect();
+
+    println!("Converting symbolic register names to decimal value");
     let mut stage_3_lines: Vec<String> = Vec::new();
     for line in &stage_2_post_labels{
         let mut new_line = line.clone().to_owned();
@@ -122,18 +127,11 @@ fn main() {
             let replace = rg.replace_all(&new_line[..], register_look_up[key].to_string());
             new_line = replace.to_string();
         }
-        println!("{} --> {}", line, new_line);
         stage_3_lines.push(new_line);
 
     }
-    // This is for already processed lines. not lines can have comments
-    // no lines can have symbolic names 
-    // valid lines:
-    //      add 3 2 1
-    // where those are the values of the registers. or in an I type
-    // it would be the calcalated immediate value
-    // invalid lines:
-    //      add $t0 $v1 $v2 # adding or something
+
+    println!("Generating binary representation");
     for line in stage_3_lines{
         line_count += 1;
         let line_as_bin = opcodes::asm_to_bin(&line);
@@ -153,6 +151,7 @@ fn main() {
         }
     }
 
+    println!("Printing binary to stdout");
     for bin in binary {
         println!("{:#010x}", bin);
     }
--- test/test.asm
+++ test/test.asm
@@ -4,6 +4,6 @@ div 3 1 4
 add 3 1 2
 BRANCH: add 1 3 4 
 
-div v1 v0 v1
+div $v1 v0 v1
 
 beq 3 3 BRANCH
